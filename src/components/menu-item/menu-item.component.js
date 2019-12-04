50135ebc33d251a3f9788b7aac31b83b51eb4993
Zhango90 <ict@MacBook-Pro-di-ict.local>
2019-12-04 10:29:27 +0100

    styling of the menu completed

---
+++ src/components/menu-item/menu-item.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './menu-item.style.scss'
+
+const MenuItem = ({title, imageUrl, size}) => (
+  <div className={`menu-item ${size}`} >
+    <div className="background-image" style={{
+      backgroundImage: `url(${imageUrl})`
+    }}>
+    </div>
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+)
+export default MenuItem
