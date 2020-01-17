be482195c92862a50210fd6f01adf0b5d45e3242
Zhango90 <ict@MacBook-Pro-di-ict.local>
2020-01-17 17:35:14 +0100

    added redux setting

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,31 +12,6 @@ const config = {
   appId: "1:607381564497:web:565e248d32b01d90bddc70"
 }
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) {
-    return 
-  }
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
-  const snapShot = await userRef.get()
-  console.log(snapShot)
-  if (!snapShot.exists) {
-    const {displayName, email} = userAuth
-    const createdAt = new Date()
-
-    try {
-      await userRef.set({
-        displayName,
-        createdAt,
-        email,
-        ...additionalData
-      })
-    } catch (error) {
-      console.log('Error in creating the user', error.message)
-    }
-  }
-  return userRef
-}
-
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
