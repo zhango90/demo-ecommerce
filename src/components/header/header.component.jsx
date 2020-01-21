29cedc71b339bf3708462989e0a3c813369fdc14
Zhango90 <ict@MacBook-Pro-di-ict.local>
2020-01-21 12:55:53 +0100

    cart icon added and cart dropdown to our application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux'
 import { auth } from '../../firebase/firebase.utils';
-
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, cartHidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -28,12 +29,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
+    {!cartHidden && <CartDropdown/>}
   </div>
 );
-
-const mapStateToProps = state => ({
-  currentUser : state.user.currentUser
+//questo è wow distructuring nested values
+const mapStateToProps = ({user:{currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
